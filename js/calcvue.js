feddec7dc6505ddd6a6dca420137201e635121f9
Adam Marshall <adam@appsynergy.net>
2015-02-28 12:09:08 +0000

    Additional comments

--- js/calcvue.js
+++ js/calcvue.js
@@ -8,6 +8,7 @@ var calculator = new Vue({
 	operators: ["+", "-"],
 	output: "",
 	operation: null,
+	// our simple app only has two buffers, so can perform one calculation at a time.
 	buffers: {
 		nom: "",
 		denom: "",
@@ -17,16 +18,22 @@ var calculator = new Vue({
   // access these methods for our click handlers
   methods: {
     operator: function (op) {
+	  // set the operation
       this.operation = op;
+	  // prepare to write to demon buffer
 	  this.openBuffer = "denom";
 	  this.buffers.denom = "";
+	  // write to screen
 	  this.output += op;
 	},
     operand: function (n) {
+	  // write characters to buffer
 	  this.buffers[this.openBuffer] += String(n);
+	  // write to screen
 	  this.output += n;
     },
 	clear: function() {
+		// wipe all buffers and output
 		this.buffers.nom = "";
 		this.buffers.denom = "";
 		this.openBuffer = "nom";
@@ -34,10 +41,13 @@ var calculator = new Vue({
 	},
 	evaluate: function() {
 		var answer = this.calculate();
+		// store the answer in the nom buffer
 		this.buffers.nom = answer;
+		// write answer to screen
 		this.output = answer;
 	},
 	calculate: function() {
+		// mathematical computations
 		switch (this.operation) {
 			case "+":
 				return (parseInt(this.buffers.nom, 10) + parseInt(this.buffers.denom, 10));
