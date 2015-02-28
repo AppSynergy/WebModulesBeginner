4ea653b7eefdea31ec1d65293e78d6803040994d
Adam Marshall <adam@appsynergy.net>
2015-02-28 10:34:33 +0000

    Add some calculator buttons, and include a Vue to handle the viewmodel logic.

---
+++ js/calcvue.js
@@ -0,0 +1,9 @@
+// create a new Vue
+var calculator = new Vue({
+  // on the element with class calculator
+  el: '.calculator',
+  // with the following data attached to the object:
+  data: {
+    numbers: [0,1,2,3,4,5,6,7,8,9]
+  }
+})
