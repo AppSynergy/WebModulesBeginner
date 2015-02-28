3ce6071dde6826df37b2a65bee4a2f4875285c8c
Adam Marshall <adam@appsynergy.net>
2015-02-28 11:42:59 +0000

    Logic for calculation and buffering. Added debug to see contents of buffers.

--- js/calcvue.js
+++ js/calcvue.js
@@ -6,15 +6,47 @@ var calculator = new Vue({
   data: {
     numbers: [0,1,2,3,4,5,6,7,8,9],
 	operators: ["+", "-"],
-	output: "."
+	output: "",
+	operation: null,
+	buffers: {
+		nom: "",
+		denom: "",
+	},
+	openBuffer: "nom"
   },
   // access these methods for our click handlers
   methods: {
     operator: function (op) {
-      this.output += op;
+      this.operation = op;
+	  this.openBuffer = "denom";
+	  this.buffers.denom = "";
+	  this.output += op;
 	},
     operand: function (n) {
+	  this.buffers[this.openBuffer] += String(n);
 	  this.output += n;
-    }
+    },
+	clear: function() {
+		this.buffers.nom = "";
+		this.buffers.denom = "";
+		this.openBuffer = "nom";
+		this.output = "";
+	},
+	evaluate: function() {
+		var answer = this.calculate();
+		this.buffers.nom = answer;
+		this.output = answer;
+	},
+	calculate: function() {
+		switch (this.operation) {
+			case "+":
+				return (parseInt(this.buffers.nom, 10) + parseInt(this.buffers.denom, 10));
+				break;
+			case "-":
+				return (parseInt(this.buffers.nom, 10) - parseInt(this.buffers.denom, 10));
+				break;
+		}
+		return "Error"
+	}
   }
 })
