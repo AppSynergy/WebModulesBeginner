15f3ba3bfdd132734baa400652465675a843d601
Adam Marshall <adam@appsynergy.net>
2015-02-28 10:50:01 +0000

    Add some operators, and a div for the output

--- js/calcvue.js
+++ js/calcvue.js
@@ -4,6 +4,7 @@ var calculator = new Vue({
   el: '.calculator',
   // with the following data attached to the object:
   data: {
-    numbers: [0,1,2,3,4,5,6,7,8,9]
+    numbers: [0,1,2,3,4,5,6,7,8,9],
+	operators: ["+", "-"]
   }
 })
